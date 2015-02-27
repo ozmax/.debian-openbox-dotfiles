0a3b0253fb1949f7ac8de97049335c01a9737a25
giorgos tsakalos <ozmax@hazard.admin.grnet.gr>
2015-02-27 17:51:58 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#dotfile folder path
+dotfiles_dir=~/.dotfiles
+
+home_files="vimrc zshrc tmux.conf gitconfig"
+for file in $home_files; do
+	ln -sfv $dotfiles_dir/$file ~/.$file
+done
+
+#install openbox configs
+openbox_files="autostart.sh menu.xml rc.xml"
+for file in $openbox_files; do
+	ln -sfv $dotfiles_dir/$file ~/.config/openbox/$file
+done
+
+#install zsh ozmaxify theme
+theme=ozmaxify.zsh-theme
+ln -sfv $dotfiles_dir/$theme ~/.oh-my-zsh/themes/$theme
+
+#install tint2 config
+file=tint2rc
+ln -sfv $dotfiles_dir/$file ~/.config/tint2/$file
