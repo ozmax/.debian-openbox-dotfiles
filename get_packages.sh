d0ea801e61aa5e049ad7288a81d37af69eb4fa36
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2015-02-27 20:04:16 +0200

    update software install

--- get_packages.sh
+++ get_packages.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 
-sudo apt-get install htop pidgin icedove vim git openbox tint2 pcmanfm\
-    pidgin-otr x11-common lightdm zsh mplayer vlc 
+sudo apt-get install htop pidgin pidgin-otr icedove vim git openbox obconf tint2 pcmanfm\
+    x11-common lightdm zsh mplayer vlc 
 
 sudo wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh\
     -O - |  sh
