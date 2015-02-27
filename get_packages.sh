90014e9e00bcf78bd1cac8a6a304bb6a5c04c527
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2015-02-27 19:24:23 +0200

    add script
    installs base software

---
+++ get_packages.sh
@@ -0,0 +1,12 @@
+#sudo apt-get update
+
+#sudo apt-get install htop pidgin icedove vim git openbox tint2 pcmanfm\
+# pidgin-otr x11-common lightdm zsh mplayer vlc 
+
+#install oh-my-zsh
+sudo wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh\ 
+-O - |  sh
+
+
+
+
