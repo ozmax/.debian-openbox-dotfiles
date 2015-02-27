55a50cada38c07a49c94e03c327c20bbdfb7bcde
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2015-02-27 19:33:57 +0200

    update package installer

--- get_packages.sh
+++ get_packages.sh
@@ -1,12 +1,9 @@
-#sudo apt-get update
+sudo apt-get update
 
-#sudo apt-get install htop pidgin icedove vim git openbox tint2 pcmanfm\
-# pidgin-otr x11-common lightdm zsh mplayer vlc 
+sudo apt-get install htop pidgin icedove vim git openbox tint2 pcmanfm\
+    pidgin-otr x11-common lightdm zsh mplayer vlc 
 
-#install oh-my-zsh
-sudo wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh\ 
--O - |  sh
+sudo wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh\
+    -O - |  sh
 
-
-
-
+echo "Done"
