8c923718536896c020a342f64797e1517a3c3627
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2015-03-02 16:01:22 +0200

    add virtualenvwrapper confs

--- get_packages.sh
+++ get_packages.sh
@@ -4,6 +4,10 @@ sudo apt-get install htop pidgin pidgin-otr icedove vim git openbox obconf tint2
     x11-common lightdm zsh mplayer vlc gmrun python-pip
 
 sudo pip install flake8 virtualenv virtualenvwrapper
+#make virtualenvwrapper configs
+export WORKON_HOME=~/Envs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
 
 sudo wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh\
     -O - |  sh
