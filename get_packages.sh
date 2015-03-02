40cdf1f3f145c14a2738beb2d6e4ab26336c2d7c
tzi ti <ozmax@kurama.(none)>
2015-03-03 01:08:36 +0200

    edit install script
    leave only base softw

--- get_packages.sh
+++ get_packages.sh
@@ -1,16 +1,16 @@
 sudo apt-get update
 
-sudo apt-get install htop pidgin pidgin-otr icedove vim openbox obconf\
-    tint2 pcmanfm x11-common lightdm zsh mplayer vlc gmrun python-pip
+#base 
+sudo apt-get install openbox obconf tint2 x11-common lightdm zsh gmrun 
 
-sudo chsh -s /bin/zsh ozmax
-sudo pip install flake8 virtualenv virtualenvwrapper
+#base tools - uncomment after base success
+#sudo apt-get install htop pidgin pidgin-otr icedove vim zsh mplayer vlc python-pip
+#sudo chsh -s /bin/zsh ozmax
+#sudo pip install flake8 virtualenv virtualenvwrapper
 #make virtualenvwrapper configs
-export WORKON_HOME=~/Envs
-mkdir -p $WORKON_HOME
-source /usr/local/bin/virtualenvwrapper.sh
-
-sudo wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh\
-    -O - |  sh
-
-echo "Done"
+#export WORKON_HOME=~/Envs
+#mkdir -p $WORKON_HOME
+#source /usr/local/bin/virtualenvwrapper.sh
+#sudo wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh\
+#    -O - |  sh
+#
